9cc55bbcad648d2f7460b43a7555939c2591e295
Eric Chin <eric.chin@liferay.com>
2020-08-31 16:58:33 -0700

    Service Worker registration script

--- modules/pwa/pwa-resources/src/main/resources/META-INF/resources/js/registration.js
+++ modules/pwa/pwa-resources/src/main/resources/META-INF/resources/js/registration.js
@@ -1,4 +1,22 @@
 $(function() {
+	const urlB64ToUint8Array = base64String => {
+		const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+		const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
+		const rawData = atob(base64)
+		const outputArray = new Uint8Array(rawData.length)
+		for (let i = 0; i < rawData.length; ++i) {
+			outputArray[i] = rawData.charCodeAt(i)
+		}
+		return outputArray
+	}
+
+	const applicationServerKey = '';
+
+	const config = {
+		userVisibleOnly: true,
+		applicationServerKey: urlB64ToUint8Array(applicationServerKey)
+	}
+
 	if ('serviceWorker' in navigator) {
 		window.addEventListener('load', function() {
 			navigator.serviceWorker.register(
@@ -11,6 +29,17 @@ $(function() {
 					console.log('ServiceWorker registration failed: ', err);
 				}
 			);
+		});
+
+		navigator.serviceWorker.ready.then(function(reg) {
+			reg.pushManager.subscribe(config).then(function(subscription) {
+				console.log('KEY:: ' + JSON.stringify(subscription));
+				console.log('ENDPOINT:: ' + subscription.endpoint);
+
+				// At this point you would most likely send the subscription
+				// endpoint to your server, save it, then use it to send a
+				// push message at a later date
+			})
 		});
 	}
 });
